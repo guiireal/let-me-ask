e14a368e0ba4daf6e172b63ef793c3699df76adc
Guilherme França <guii_real@hotmail.com>
2021-06-22 01:39:00 -0300

    feat: login with Google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,16 +4,15 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import { useAuth } from "../hooks/useAuth";
 import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
 
 export function Home() {
   const history = useHistory();
 
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
